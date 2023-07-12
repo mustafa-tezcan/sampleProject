aebf5e6d1919ed601a934642d05a59f9923bdd45
mustafa tezcan <mustafatzcn44@gmail.com>
2023-07-12 12:22:18 +0300

    ilk kayit

---
+++ src/sampleProject/Main.java
@@ -0,0 +1,14 @@
+package sampleProject;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		
+		
+		System.out.println("12 temmuz 2023");
+		
+		System.out.println("bu örnek bir projedir.");
+		
+	}
+
+}
